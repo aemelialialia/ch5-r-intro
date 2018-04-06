fad603636a88e3e969a792be9b9d8f142e67a1f3
Emily Zhang <emily.aiwen@gmail.com>
2018-04-06 14:52:37 -0700

    Added practice code and lines for R exercises

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -16,7 +16,8 @@ puppies <- 3
 puppy_price <- 50
 
 # Create a variable `total_cost` that has the total cost of all of your puppies
-toatl_cost <- (puppies * puppy_price)
+total_cost <- (puppies * puppy_price)
+total_cost
 
 # Create a boolean variable `too_expensive`, set to TRUE if the cost is greater 
 # than $1,000 <- 
